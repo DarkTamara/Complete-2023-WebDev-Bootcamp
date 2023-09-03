e956c4faf97691db36405d8e72ec64528de11f3f
AndreeaM <miron.andreea12345@gmail.com>
2023-09-03 18:05:31 +0300

    More work on the To Do list

--- Capstone/To Do List App/index.js
+++ Capstone/To Do List App/index.js
@@ -6,6 +6,9 @@ const port = 3000;
 const app = express();
 
 
+//functions
+
+
 //middleware 
 app.use(express.static("public")); // static files import 
 app.use(bodyParser.urlencoded({ extended: true })); //parse body 
@@ -13,8 +16,25 @@ app.use(bodyParser.urlencoded({ extended: true })); //parse body
 
 //requests 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs",{ listsNames : listsNames, listTasks :  listTasks_1});
 });
+
+app.get("/dailyTasks_list", (req, res) => {
+    res.render("index.ejs",{ listsNames : listsNames, listTasks :  listTasks_1});
+});
+
+app.get("/workTasks_list", (req, res) => {
+    res.render("index.ejs",{ listsNames : listsNames, listTasks :  listTasks_2});
+});
+
+app.get("/toDo_list", (req, res) => {
+    res.render("index.ejs",{ listsNames : listsNames, listTasks :  listTasks_3});
+});
+
+app.get("/add", (req, res) => {
+    res.render("editModal.ejs");
+});
+
 
 
 
@@ -22,3 +42,45 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
   });
+
+
+//database 
+var listTasks_1 = [
+    {   title:"task 1",
+        info: "info 1"},
+    {   title:"task 1",
+        info: "info 1"}   
+];
+var listTasks_2 = [
+    {   title:"task 1",
+        info: "info 1"},
+    {   title:"task 1",
+        info: "info 1"},
+    {   title:"task 1",
+        info: "info 1"},
+    {   title:"task 1",
+        info: "info 1"} 
+];
+var listTasks_3 = [
+    {   title:"task 1",
+        info: "info 1"},
+    {   title:"task 1",
+        info: "info 1"},
+    {   title:"task 1",
+        info: "info 1"} 
+];
+const listsNames = 
+    [
+        {   listName :"Daily Tasks",
+            id: "dailyTasks_list",
+            name : "dailyTasks_list",
+            value : "/dailyTasks_list"  },
+        {   listName :"Work Tasks",
+            id: "workTasks_list",
+            name : "workTasks_list",
+            value : "/workTasks_list"  },
+        {   listName :"To do",
+            id: "toDo_list",
+            name : "toDo_list",
+            value : "/toDo_list"   },
+    ];
